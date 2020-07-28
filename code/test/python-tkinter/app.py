c1e1d8a7c87cf1a99ded4dd5a7bde1f5a1e52e3a
rodrigo <rodrigodouglasba@gmail.com>
2020-07-28 19:46:34 -0300

    some updates to my GUI

--- code/test/python-tkinter/app.py
+++ code/test/python-tkinter/app.py
@@ -1,5 +1,7 @@
-import tkinter
+import tkinter as tk
+from tkinter import ttk
 
+i = 0
 def capture():
     print('Capture Mode')
     lblMenu['text'] = 'capture'
@@ -8,8 +10,24 @@ def liveView():
     print('Live View Mode')
     lblMenu['text'] = 'LIVE'
 
+def sendButtonPressed():
+    print('Button pressed')
+    label_command['text'] = 'Button pressed!'
+    nCapture = entry_capture.get()
+    nSamples = entry_sample.get()
+    sampleRate = entry_rate.get()
 
-mainWindow = tkinter.Tk()
+    print(nCapture)
+    print(nSamples)
+    print(sampleRate)
+
+    global i
+    i += 1
+    print(i)
+    bar_var.set(i)
+ 
+
+mainWindow = tk.Tk()
 mainWindow.title("PIBITI-2019")
 
 #dimensoes da janela
@@ -20,8 +38,8 @@ screenHeight = mainWindow.winfo_screenheight()
 posx =screenWidth/2 - windowWidth/2 
 posy =screenHeight/2 - windowHeight/2
 
-mainWindow.geometry("%dx%d+%d+%d" % (windowWidth,windowHeight,posx,posy))
-mainWindow.minsize(width = windowWidth, height = windowHeight)
+#mainWindow.geometry("%dx%d+%d+%d" % (windowWidth,windowHeight,posx,posy))
+#mainWindow.minsize(width = windowWidth, height = windowHeight)
 
 #aparencia
 
@@ -35,9 +53,9 @@ mainWindow.iconbitmap("code/resources/icon/if.ico")
 
 #inicializacao DE WIDGETS
 '''
-btnCapture = tkinter.Button(mainWindow, text = "Capture", command = capture)
-btnLive    = tkinter.Button(mainWindow, text = "Live View", command = liveView)
-lblMenu    = tkinter.Label(
+btnCapture = tk.Button(mainWindow, text = "Capture", command = capture)
+btnLive    = tk.Button(mainWindow, text = "Live View", command = liveView)
+lblMenu    = tk.Label(
                 mainWindow,
                 text = "What do you want to do?", 
                 relief = "groove",
@@ -48,37 +66,76 @@ lblMenu    = tkinter.Label(
                 anchor = 'w')
 '''
 #label
-tkOperation    = tkinter.Label(mainWindow, bg = "blue",text="Operation").grid(row=0,column=0,sticky="we")
+
 #
 
 
 #-------------------
 #widgets
 
-tkProprierties = tkinter.Label(mainWindow, bg = "green",text="Proprierties").grid(row=0,column=1,sticky="we")
+##OPERATION frame
 
-    #tkLabelCapt = tkinter.Label(mainWindow,text="Number of Captures:")
-    #tkEntryCapt = tkinter.Entry(mainWindow)
-    #tkLabelSample = tkinter.Label(mainWindow,text="Number of Samples:")
-    #tkEntrySample = tkinter.Entry(mainWindow)
-    #tkLabelRate = tkinter.Label(mainWindow,text="Sample Rate(ms):")
-    #tkEntryRate = tkinter.Entry(mainWindow)
+frame_operation = tk.Frame(mainWindow)
 
-tkAdditional   = tkinter.Label(mainWindow, bg = "gray",text="Additional").grid(row=1,column=0,sticky="we")
+### widgets
 
-    #tkButtonCommand = tkinter.Button(mainWindow,text="SEND COMMAND",command = sendModeToSensor)
+label_operation = tk.Label(frame_operation, bg = "blue",text="Operation")
 
-tkTerminal     = tkinter.Label(mainWindow, bg = "red",text="Terminal").grid(row=1,column=1,sticky="we")
+label_capture = tk.Label(frame_operation,text="Number of Captures:")
+entry_capture = tk.Entry(frame_operation)
+label_sample = tk.Label(frame_operation,text="Number of Samples:")
+entry_sample = tk.Entry(frame_operation)
+label_rate = tk.Label(frame_operation,text="Sample Rate(ms):")
+entry_rate = tk.Entry(frame_operation)
 
-#-------------------
-#GRID
+### layout
+
+label_operation.grid(sticky="we",rowspan=2)
+
+label_capture.grid(sticky='we')
+entry_capture.grid(sticky='we')
+label_sample.grid(sticky='we')
+entry_sample.grid(sticky='we')
+label_rate.grid(sticky='we')
+entry_rate.grid(sticky='we')
 
 
-#-------------------
-#pack
+##Graphic Frame
 
-##lblMenu.pack()
-##btnCapture.pack()
-#btnLive.pack()
+
+##ADDITIONAL Frame
+frame_additional = tk.Frame(mainWindow)
+
+### widgets
+label_additional = tk.Label(frame_additional,text = "ADDITIONAL",bg = 'blue')
+
+button_command = tk.Button(frame_additional,text="SEND\nCOMMAND",command = sendButtonPressed)#lambda: sendButtonPressed(i))
+label_command = tk.Label(frame_additional,text = '\n\n\n')
+label_progress = tk.Label(frame_additional,text ='Progress')
+
+bar_var = tk.DoubleVar()
+bar_var.set(i)
+bar_progress = ttk.Progressbar(frame_additional,variable=bar_var,maximum=10)
+
+### layout
+label_additional.grid(sticky='we',rowspan=2)
+
+button_command.grid(sticky="we",rowspan=2)
+label_command.grid(sticky='we')
+
+label_progress.grid(sticky='we')
+bar_progress.grid(sticky='we')
+
+##TERMINAL ZONE
+
+#tkTerminal     = tk.Label(mainWindow, bg = "red",text="Terminal")
+
+#tkTerminal.grid(row=6,column=1,sticky="nswe",rowspan=3)
+
+#tkAdditional.grid(row=6,column=0,sticky="we")
+
+## GERAL
+frame_operation.grid()
+frame_additional.grid()
 
 mainWindow.mainloop()
